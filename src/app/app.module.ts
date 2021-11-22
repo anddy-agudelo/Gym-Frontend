34042d4758914cd42d7d83661db20f9b490ce15f
anddyagudelo <anddyagudelo@outlook.com>
2021-11-21 20:27:03 -0500

    Agregando Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './autenticar/login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ejercicio/ejercicio.module.ts
+++ src/app/ejercicio/ejercicio.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListarComponent } from './listar/listar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarComponent
+  ],
   imports: [
     CommonModule
   ]
